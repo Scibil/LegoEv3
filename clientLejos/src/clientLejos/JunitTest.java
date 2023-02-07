b562e36abb0fa9425babfba746f0c8238ce2f4c5
Esame <Esame@LEGO-7>
2023-02-07 09:23:37 +0100

    client

---
+++ clientLejos/src/clientLejos/JunitTest.java
@@ -0,0 +1,18 @@
+package clientLejos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JunitTest {
+
+	@Test
+	public void test() {
+		Client client = new Client();
+		
+        //boolean connected = client.connessione();
+        //assertTrue(connected);
+		
+	}
+
+}
